8dbc6588d6db04b802e0ea248388621d75550b2e
Zuri <zuri@publishsoft.io>
2021-07-28 16:12:44 +0900

    #3 STATE_#3.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 
 class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   console.log("hello");
+  // }
+
   state = {
     // state == object
     // component의 data를 넣을 공간이 있고 이 데이터는 변하는
@@ -23,7 +27,22 @@ class App extends React.Component {
     this.setState(current => ({ count: current.count - 1 }));
   };
 
+  componentDidMount() {
+    console.log("Component rendered");
+  }
+
+  componentDidUpdate() {
+    // onClick 으로 숫자가 변하게 만든거 자체가 update
+    console.log("I just updated");
+  }
+
+  componentWillUnmount() {
+    // preserve log 체크 후 reload하면 볼 수 있음
+    console.log("GoodBye, cruel world");
+  }
+
   render() {
+    console.log("I'm rendering");
     return ( 
       <div>
         <h1>The number is : {this.state.count}</h1>
