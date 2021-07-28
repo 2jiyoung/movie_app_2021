98dde0f3a3ab93b11ee22cb0bbbc1ae22660e1a9
Zuri <zuri@publishsoft.io>
2021-07-28 15:58:33 +0900

    #3STATE_#3.1 All you need to know about state

--- src/App.js
+++ src/App.js
@@ -1,86 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import Potato from "./Potato";
-
-/*
-  **jsx = HTML + JavaScript
-  react가 좋은 이유는 재사용 가능한 component를 만들 수 있다는 점 
-  => component를 계속해서 반복해서 사용할 수 있는 것
-  component는 대문자로 시작해야함.
-  component로 정보를 보낼 수 있음.
-*/
-
-// function Food({ fav }) {
-//   // props.fav = { fav }
-//   return <h1>I like {fav}</h1>;
-// }
 
 
-const foodILike = [
-  {
-    id: 1,
-    name: "김치",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    id: 2,
-    name: "삼겹살",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "비빔밥",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.8
-  },
-  {
-    id: 4,
-    name: "돈까스",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 5.5
-  },
-  {
-    id: 5,
-    name: "김밥",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.7
+class App extends React.Component {
+  state = {
+    // state == object
+    // component의 data를 넣을 공간이 있고 이 데이터는 변하는
+    count: 0
+  };
+
+  add = () => {
+    console.log('add');
+    // this.state.count = 1;
+    // this.setState({ count: this.state.count + 1 });
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+
+  minus = () => {
+    console.log('minus');
+    // this.state.count = -1;
+    // this.setState({ count: this.state.count -1 });
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+
+  render() {
+    return ( 
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>+add</button>
+        <button onClick={this.minus}>-minus</button>
+      </div>
+    );
   }
-];
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
 }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  )};
 
   export default App;
