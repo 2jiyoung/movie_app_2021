09f43522dc6cf76aa4623da9274882d9c435f55e
Zuri <zuri@publishsoft.io>
2021-07-29 13:32:40 +0900

    #4 Making the movie app_#4.1 Rendering the movie

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
+  };
+   
+  getMovies = async () => {
+    // await : axio가 끝날 때까지 기다렸다가 계속하는
+    // ↓ 아래랑 같은 형태 = const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{ this.state.isLoading ? "Loading..." : "We are ready" }</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+              ))}
+      </div>
+    );
   }
-
 }
+
 
 
   export default App;
